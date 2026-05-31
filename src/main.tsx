6461e5ec7336d3c308fb6bbc8008ec8251d5a955
AnnaDzig <adzyhota@protonmail.com>
2026-05-31 14:53:49 +0200

    Provide TanStack Query client to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./App.tsx";
+import { queryClient } from "./lib/queryClient";
+import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import ThemeProvider from "./context/ThemeProvider";
 import AboutPage from "./pages/AboutPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import ProductDetails from "./pages/ProductDetails.tsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <ThemeProvider>
-        <BrowserRouter basename="/react-class-components-search/">
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route index element={<HomePage />} />
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter basename="/react-class-components-search/">
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route index element={<HomePage />} />
 
-              <Route path="products/:productId" element={<HomePage />}>
-                <Route index element={<ProductDetails />} />
+                <Route path="products/:productId" element={<HomePage />}>
+                  <Route index element={<ProductDetails />} />
+                </Route>
               </Route>
-            </Route>
 
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </ErrorBoundary>
   </StrictMode>,
